630b46699f270ed6d4a84363d0deabdd8ef8eebc
Marcus Camargo <marcus@agenciavisia.com.br>
2020-01-12 13:05:53 -0300

    AppBar / RaiseButton / State

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,57 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion(){
+    setState(() {
+      questionIndex++;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions.elementAt(questionIndex)),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
